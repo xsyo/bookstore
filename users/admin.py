708f5e9f96f1e568fd5d5ca3de11f70d3e39da00
xsyo <xsyu1324@mail.ru>
2020-02-05 18:08:51 +0600

    ch3

---
+++ users/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import CustomUserChangeFrom, CustomUserCreationForm
+
+CustomUser = get_user_model()
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeFrom
+    model = CustomUser
+    list_display = ['email', 'username',]
+
+admin.site.register(CustomUser, CustomUserAdmin)
