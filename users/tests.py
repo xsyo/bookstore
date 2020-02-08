d681e0a05ab942329e9daaaffbd9cba69467bdcc
xsyo <xsyu1324@mail.ru>
2020-02-08 15:00:58 +0600

    ch10

--- users/tests.py
+++ users/tests.py
@@ -38,12 +38,12 @@ class SignUpPageTests(TestCase):
     email = 'newuser@email.com'
     
     def setUp(self):
-        url = reverse('signup')
+        url = reverse('account_signup')
         self.response = self.client.get(url)
 
     def test_signup_template(self):
         self.assertEqual(self.response.status_code, 200)
-        self.assertTemplateUsed(self.response, 'accont/signup.html')
+        self.assertTemplateUsed(self.response, 'account/signup.html')
         self.assertContains(self.response, 'Sign Up')
         self.assertNotContains(self.response, 'Hi there! I should not be on the page.')
 
